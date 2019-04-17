4dd957041ebaed94c73f9aa7ee125f338fc3c070
Edoardo Carraro <edoz96@gmail.com>
2019-04-17 17:23:56 +0200

    aggiornamento ste/edo discretizzazione

--- Vehicle_Project_Main.m
+++ Vehicle_Project_Main.m
@@ -25,6 +25,9 @@ psi     =       0;          % yaw angle (rad)
 r       =       0;          % yaw rate (rad/s)
 xi0     =       [X Y Ux beta psi r]';
 
+xi_sim(1,1:length(xi0)) = xi0;
+
+
 %% Step amplitude and input vector
 Td_step             =       80; % for now - random number
 delta_step          =       50; % for now - random number
@@ -41,7 +44,7 @@ boundary_number     =       1;
 tau                 =       0;
 d                   =       0;
 
-for i = 1:n_iterations %end of the iteration when we reach the final wl
+for i = 2:n_iterations %end of the iteration when we reach the final wl
   
   % current wayline selection  
   
@@ -68,27 +71,29 @@ for i = 1:n_iterations %end of the iteration when we reach the final wl
   end
   
   % from the output of the interpolator to the new state
-  [xi_dot,Forces] = Vehicle_Model_Function(tau,xi0,u_output,d,theta);
+  xi_sim(:,i) = xi_sim(:,i-1) + Ts*vehicle(0, xi_sim(:,i-1),u_output, 0, theta);
+%   [xi_dot,Forces] = Vehicle_Model_Function(tau,xi0,u_output,d,theta);
   
   % update of the target position
-  x_target   = xi0(1) + xi_dot(3)*cos(xi_dot(4));
-  y_target   = xi0(2) + xi_dot(3)*sin(xi_dot(4));
+%   x_target   = xi0(1) + xi_dot(3)*cos(xi_dot(4));
+%   y_target   = xi0(2) + xi_dot(3)*sin(xi_dot(4));
+% LA POSIZIONE E' ALL'INTERNO DEL VETTORE XI
   
   % check if the target position is feasible
-  [inside,boundary_number] = track_constraints(x_target,y_target,N,innerBoundary,outerBoundary);
+  [inside,boundary_number] = track_constraints(xi_sim(1,i),xi_sim(2,i),N,innerBoundary,outerBoundary);
   
   % if the target position is feasible, update of the state with the new
   % position. else exit the for cycle, because the trajectory is not
   % feasible.
   if inside == 1
-      plot([x_target xi0(1)],[y_target xi0(2)]);
+      plot([xi_sim(1,i) xi_sim(1,i-1)],[xi_sim(2,i) xi_sim(2,i-1)]);
       pause(.001)
-      xi0(1) = x_target;
-      xi0(2) = y_target;
-      xi0(3) = xi_dot(3);
-      xi0(4) = xi_dot(4);
-      xi0(5) = xi_dot(5);
-      xi0(6) = xi_dot(6);
+%       xi0(1) = x_target;
+%       xi0(2) = y_target;
+%       xi0(3) = xi_dot(3);
+%       xi0(4) = xi_dot(4);
+%       xi0(5) = xi_dot(5);
+%       xi0(6) = xi_dot(6);
   else
       return
   end
