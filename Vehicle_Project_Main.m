56414d9e6adc4e17a8d61086ae4d06e872533bb4
SDZArnold <stefano.di.zenobio@gmail.com>
2019-04-17 11:15:51 +0200

    Aggiornamento allo script di Sofia_2: Funzione interpolator implementata

--- Vehicle_Project_Main.m
+++ Vehicle_Project_Main.m
@@ -7,13 +7,13 @@ clc
 
 %% track generation and waypoints positioning
 
-[track,innerBoundary,outerBoundary,x0,y0,N] = track_generation();
+[track,innerBoundary,outerBoundary,x0,y0,N] =       track_generation();
 
 % This function has to return two vectors containing the inner point and
 % the outer one of each wayline.
-[ inner_wl, outer_wl ]                      = waylines_selector();
+[ inner_wl, outer_wl ]                      =       waylines_selector();
 
-n_l = length(inner_wl);
+n_wl                                        =       length(inner_wl);
 
 %% parameters initialization and setting of initial state
 run('Parameters.m');
@@ -47,14 +47,24 @@ for i = 1:n_iterations
   
   current_wl        =       current_wayline();
   
-  % interpolator
+  if (current_wl == n_wl)
+  % If we are in correspondence of the last wayline we have to keep the
+  % optimal values untill we reach the end of the track, otherwise we'll
+  % have computational problem.
+      u_output          =           [T_opt(current_wl);delta_opt(current_wl)];
   
-  d_interpolated    =       interpolator_bws( inner_wl(current_wl),outer_wl(current_wl),xi0(1),xi0(2));
+  else
+      
+      % interpolator
+
+      d_interpolated    =       interpolator_bws( inner_wl(current_wl),outer_wl(current_wl),inner_wl(current_wl+1),outer_wl(current_wl+1),xi0(1),xi0(2));
+
+      T_kp1             =       T_opt(current_wl) + (T_opt(current_wl+1) - T_opt(current_wl))*d_interpolated;
+      delta_kp1         =       delta_opt(current_wl) + (delta_opt(current_wl+1) - delta_opt(current_wl))*d_interpolated;
+
+      u_output          =       [T_kp1;delta_kp1];
   
-  T_kp1             =       T_opt(current_wl) + (T_opt(current_wl+1) - T_opt(current_wl))*d_interpolated;
-  delta_kp1         =       delta_opt(current_wl) + (delta_opt(current_wl+1) - delta_opt(current_wl))*d_interpolated;
-  
-  u_output          =       [T_kp1;delta_kp1];
+  end
   
   % from the output of the interpolator to the new state
   [xi_dot,Forces] = Vehicle_Model_Function(tau,xi0,u_output,d,theta);
@@ -64,7 +74,7 @@ for i = 1:n_iterations
   y_target   = xi0(2) + xi_dot(3)*sin(xi_dot(4));
   
   % check if the target position is feasible
-  inside = track_constraints(x_target,y_target,N,innerBoundary,outerBoundary)
+  inside = track_constraints(x_target,y_target,N,innerBoundary,outerBoundary);
   
   % if the target position is feasible, update of the state with the new
   % position. else exit the for cycle, because the trajectory is not
