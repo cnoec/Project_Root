4ed96a43768fe63b9eb69f8a7d10d702239f79cb
SDZArnold <stefano.di.zenobio@gmail.com>
2019-04-19 11:38:20 +0200

    Aggiornamento_1 simulatore

--- Vehicle_Project_Main.m
+++ Vehicle_Project_Main.m
@@ -13,16 +13,17 @@ max_distance = 100;
 
 aux1 = zeros(N,3);
 aux2 = zeros(N,3);
+
 for i=1:N
-   aux1(i,:) = innerBoundary(N-i+1,:); 
+   aux1(i,:) = outerBoundary(N-i+1,:); 
    if(i==N)
-       aux2(i,:) = outerBoundary(1,:);
+       aux2(i,:) = innerBoundary(1,:);
    else
-       aux2(i,:) = outerBoundary(i+1,:);
+       aux2(i,:) = innerBoundary(i+1,:);
    end
 end
-innerBoundary = aux1;
-outerBoundary = aux2;
+innerBoundary = aux2;
+outerBoundary = aux1;
 clear aux1 aux2 
 
 % This function has to return two vectors containing the inner point and
@@ -39,11 +40,14 @@ end
 %% parameters initialization and setting of initial state
 run('Parameters.m');
 
+m = (outerBoundary(1,2)-innerBoundary(1,2))/(outerBoundary(1,1)-innerBoundary(1,1));
+m = -1/m;
+
 X       =       x0;         % inertial X position (m)
 Y       =       y0;         % inertial Y position (m)
-Ux      =       -80;          % body x velocity (m/s)
-beta    =       -9;          % sideslip angle (rad)
-psi     =       0;          % yaw angle (rad)
+Ux      =       20;          % body x velocity (m/s)
+beta    =       0;          % sideslip angle (rad)
+psi     =       atan(m);          % yaw angle (rad)
 r       =       0;          % yaw rate (rad/s)
 xi0     =       [X Y Ux beta psi r]';
 
@@ -57,49 +61,65 @@ u_output            =       [Td_step;delta_step];
 
 %% trajectory optimizer
 
+T_opt               =       [Tdmax/10;
+                             Tdmax/10;
+                             Tdmax/10;
+                             Tdmax/10;
+                             Tdmax/10;
+                             Tdmax/10;];
+                         
+delta_opt           =       [0;
+                             pi*7/4;
+                             pi/4;
+                             150;
+                             150;
+                             150;];
+
 % [T_opt, delta_opt]  =       optimizer();
 
 %% simulation
 
-n_iterations        =       N-1;
+n_iterations        =       300;
 boundary_number     =       1;
 tau                 =       0;
 d                   =       0;
 Ts                  =       1e-2;  
 
 for i = 2:n_iterations %end of the iteration when we reach the final wl
-  
+  i
   % current wayline selection  
-  current_wl    =     current_wayline(inner_wl,outer_wl,boundary_number,innerBoundary,outerBoundary,n_wl,N)
-  
+%   current_wl    =     current_wayline(inner_wl,outer_wl,boundary_number,innerBoundary,outerBoundary,n_wl,N)
+    current_wl=1;
   
 %   if (current_wl == n_wl)
-%   % If we are in correspondence of the last wayline we have to keep the
-%   % optimal values until we reach the end of the track, otherwise we'll
-%   % have computational problem.
+  % If we are in correspondence of the last wayline we have to keep the
+  % optimal values until we reach the end of the track, otherwise we'll
+  % have computational problem.
 %       u_output          =           [T_opt(current_wl);delta_opt(current_wl)];
-%   
+  
 %   else
-%       
-%       % interpolator
-% 
-%       d_interpolated    =       interpolator_bws( inner_wl(current_wl,:),outer_wl(current_wl,:),inner_wl(current_wl+1,:),outer_wl(current_wl+1,:),xi0(1),xi0(2));
-% 
-%       T_kp1             =       T_opt(current_wl) + (T_opt(current_wl+1) - T_opt(current_wl))*d_interpolated;
-%       delta_kp1         =       delta_opt(current_wl) + (delta_opt(current_wl+1) - delta_opt(current_wl))*d_interpolated;
-% 
-%       u_output          =       [T_kp1;delta_kp1];
-%   
+      
+      % interpolator
+
+      d_interpolated    =       interpolator_bws( inner_wl(current_wl,1:2),outer_wl(current_wl,1:2),inner_wl(current_wl+1,1:2),outer_wl(current_wl+1,1:2),xi_sim(1,i-1),xi_sim(2,i-1))
+
+      T_kp1             =       T_opt(current_wl) + (T_opt(current_wl+1) - T_opt(current_wl))*d_interpolated;
+      delta_kp1         =       delta_opt(current_wl) + (delta_opt(current_wl+1) - delta_opt(current_wl))*d_interpolated;
+
+      u_output          =       [T_kp1;delta_kp1]
+  
 %   end
-%   
+  
   % from the output of the interpolator to the new state
   xi_sim(:,i) = xi_sim(:,i-1) + Ts*Vehicle_Model_Function(0, xi_sim(:,i-1),u_output, 0, theta);
+  
   % check if the target position is feasible
-  [inside,boundary_number] = track_constraints(xi_sim(1,i),xi_sim(2,i),N,innerBoundary,outerBoundary,boundary_number);
+  [inside,boundary_number] = track_constraints(xi_sim(1,i),xi_sim(2,i),N,innerBoundary,outerBoundary,boundary_number)
  
   % if the target position is feasible, update of the state with the new
   % position. else exit the for cycle, because the trajectory is not
   % feasible.
+  
   if inside == 1
       plot([xi_sim(1,i) xi_sim(1,i-1)],[xi_sim(2,i) xi_sim(2,i-1)]);
       pause(.001)
